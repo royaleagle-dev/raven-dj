3f436ef972a74de78a3f40deb4236d6b12ab528b
= <royaleagle.dev@gmail.com>
2020-12-25 19:22:11 +0100

    raven 1.0

--- invoice/urls.py
+++ invoice/urls.py
@@ -5,4 +5,5 @@ app_name = 'invoice'
 
 urlpatterns = [
 	path('', views.IndexView.as_view(), name = 'index'),
+	path('addInvoice/', views.addInvoice, name = 'addInvoice'),
 ]
--- invoice/views.py
+++ invoice/views.py
@@ -3,6 +3,7 @@ from invoice.models import Invoice
 from django.utils import timezone
 from django.views.generic import ListView
 from django.views import View
+from django.contrib import messages
 
 class IndexView(View):
 	
@@ -15,4 +16,29 @@ class IndexView(View):
 
 	def post(self, request):
 		pass
+
+def addInvoice(request):
+	if request.method == "POST":
+		heading = request.POST.get('heading')
+		amount = request.POST.get('amount')
+		email = request.POST.get('email')
+		transactionDate = request.POST.get('transactionDate')
+		status = request.POST.get('status')
+		description = request.POST.get('description')
+		issuer = request.user.username
+		date = request.POST.get('')
+
+		status = int(status)
+		if status == 1:
+			status = 'p'
+		else:
+			status = 'u'
+
+		new_invoice = Invoice(heading=heading, amount_due=amount, email=email, transaction_description=description,
+			transaction_date=transactionDate, issuer=issuer, status=status)
+
+		new_invoice.save()
+
+		messages.success (request, "Invoice successfully Added")
+		return redirect("invoice:index")
 
